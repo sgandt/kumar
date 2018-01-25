89997d0fc88cb616e6f55fcc0dbce8a1b820036c
sgandt <35653041+sgandt@users.noreply.github.com>
2018-01-25 10:56:16 +0000

    Rename sg.c to gs.c

---
+++ gs.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char z;
+printf("enter a character");
+scanf("%d",&z);
+if(z=='a'||z=='e'||z=='i||z=='o'||z=='u')
+printf("the character is vowels");
+else
+printf("the character is consonent");
+getch();
+}
